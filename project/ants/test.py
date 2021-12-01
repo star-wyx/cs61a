953083c00f338926058723ea9fa8bf0524e97dc1
star_wyx <576715206@qq.com>
2021-12-02 00:48:32 +0800

    project ants

---
+++ project/ants/test.py
@@ -0,0 +1,17 @@
+from ants import *
+hive, layout = Hive(AssaultPlan()), dry_layout
+dimensions = (1, 9)
+colony = AntColony(None, hive, ant_types(), layout, dimensions)
+laser = LaserAnt()
+ant = HarvesterAnt(2)
+bee1 = Bee(2)
+bee2 = Bee(2)
+bee3 = Bee(2)
+bee4 = Bee(2)
+colony.places["tunnel_0_0"].add_insect(laser)
+colony.places["tunnel_0_0"].add_insect(bee4)
+colony.places["tunnel_0_3"].add_insect(bee1)
+colony.places["tunnel_0_3"].add_insect(bee2)
+colony.places["tunnel_0_4"].add_insect(ant)
+colony.places["tunnel_0_5"].add_insect(bee3)
+laser.action(colony)
