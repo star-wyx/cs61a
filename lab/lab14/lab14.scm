59273da6427a87d93cbeb9f23fe9c8976e7e5f2e
star_wyx <576715206@qq.com>
2021-12-06 21:22:32 +0800

    lab14

---
+++ lab/lab14/lab14.scm
@@ -0,0 +1,26 @@
+; Lab 14: Final Review
+
+(define (compose-all funcs)
+  ;'YOUR-CODE-HERE
+  (if (null? funcs)
+    (lambda (x) x)
+    (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x))))
+)
+
+(define (has-cycle? s)
+  (define (pair-tracker seen-so-far curr)
+    (cond ((null? (cdr-stream curr)) #f)
+          ((contains? seen-so-far (car curr)) #t)
+          (else (pair-tracker (cons (car curr) seen-so-far) (cdr-stream curr)))))
+
+  (pair-tracker () s)
+)
+
+(define (contains? lst s)
+  ;'YOUR-CODE-HERE
+  (if (null? lst)
+    #f
+    (if (equal? (car lst) s)
+        #t
+        (contains? (cdr lst) s)))
+)
